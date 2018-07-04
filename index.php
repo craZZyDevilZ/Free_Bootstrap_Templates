6de65b91edd7c504a081f0f88187dc67aad628c4
Karan Aggarwal <40819803+craZZyDevilZ@users.noreply.github.com>
2018-07-04 19:29:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!-- Theme Created By Karan Aggarwal -->
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+  
+  <!-- Custom Styling -->
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <title>craZZy_DevilZ</title>
+</head> 
+<body>
+  <nav class="navbar navbar-expand-md navbar-light warning sticky-top p-1">
+    <div class="container">
+      <a href="index.php" class="navbar-brand">CraZZy_DevilZ</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ResponsiveNavBar"><span><i class="fas fa-bars"></i></span>
+      </button>
+      <div class="collapse navbar-collapse" id="ResponsiveNavBar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#aboutScroll" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#servicesScroll" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="#teamScroll" class="nav-link">Team</a>
+          </li>
+          <li class="nav-item">
+            <a href="#contactScroll" class="nav-link">Contact</a>
+          </li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid m-0 p-0">
+  <div class="container jumbotron-content">
+    <h1 class="text-center display-5 p-3">Welcome to the <span class="jumbotron-navbar-brand">CraZZy_DevilZ</span></h1>
+    <p class="lead text-center">We are a full service integrated creative agency</p>
+  </div>
+</div>
+
+<!-- About us -->
+<div id="aboutScroll" class="container-fluid">
+  <div class="row about-us-container justify-content-center">
+    <div class="col-11 col-sm-10 col-md-6 p-0 p-sm-0 p-md-3 p-lg-5"><img class="img-fluid" src="img/about-us.jpg" /></div>
+    <div class="col-11 col-sm-10 col-md-6 p-md-3 p-lg-5 about-us-content mt-1">
+      <h3 class="pb-md-1 pb-lg-3">About Us</h3>
+      <p class="lead text-justify mr-md-0 mr-lg-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tempora harum, eligendi excepturi natus ut, quas, omnis iusto et quod a at voluptas dolorem perspiciatis vero itaque fugiat. Illo, provident.
+      </p>
+      <a class="btn bg-danger btn-sm mt-md-0 mt-lg-2" href="#" role="button">Read More</a>
+    </div>
+  </div>
+</div>
+
+<!-- Our Services -->
+<div id="servicesScroll">
+<div class="container">
+  <div class="row services">
+    <div class="col">
+      <h1 class="text-center mt-3"><span>Our Services</span></h1>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row text-center mt-3 mt-sm-5 three-col-container">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-tv fa-4x mb-3"></i> 
+      <h3 class="mb-3 text-danger">Web Design & Development</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-cogs fa-4x mb-3 seo"></i> 
+      <h3 class="mb-3 text-danger seo">Search Engine Optimization</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p> 
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-cannabis fa-4x mb-3"></i> 
+      <h3 class="mb-3 text-danger">Graphic Designing</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p> 
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-chart-line fa-4x mb-3"></i> 
+      <h3 class="mb-3 text-danger">Social Media Marketing</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p> 
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-infinity fa-4x mb-3"></i> 
+      <h3 class="mb-3 text-danger">Logo Designing</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p> 
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 service-conatiner">
+      <i class="fas fa-object-group fa-4x mb-3"></i> 
+      <h3 class="mb-3 text-danger">Flex Designing</h3>
+      <p class="small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quos quas qui! Ad, enim, accusantium!</p> 
+    </div>
+  </div>
+</div>
+</div>
+
+<!--Section info -->
+<div class="container-fluid section-info mt-3">
+  <div class="container">
+    <div class="row text-center p-3">
+      <div class="col p-3">
+        <h3>30</h3>
+        <p class="small mb-0 ">Happy Customers</p>
+      </div>
+      <div class="col p-3">
+        <h3>1080</h3>
+        <p class="small mb-0">Working Hours</p>
+      </div>
+      <div class="col p-3">
+        <h3>12</h3>
+        <p class="small mb-0">Projects Completed</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Team -->
+<div id="teamScroll">
+<div  class="container">
+  <div class="row team">
+    <div class="col">
+      <h1 class="text-center mt-4 mb-5"><span>Meet the Team</span></h1>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-10 col-md-4">
+      <div class="card">
+        <img src="img/team1.jpg" alt="" class="card-img-top" />
+        <div class="card-body">
+          <h3 class="card-title">Karan Aggarwal</h3>
+          <p class="card-text">Web Designer/Developer</p>
+          <p class="small text-justify m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta velit libero.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-10 col-md-4">
+      <div class="card">
+        <img src="img/team2.jpg" alt="" class="card-img-top" />
+        <div class="card-body">
+          <h3 class="card-title">Dhirender Pratap Rana </h3>
+          <p class="card-text">SEO/SEM Manager</p>
+          <p class="small text-justify m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta velit libero.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-10 col-md-4">
+      <div class="card">
+        <div class="card-img-container">
+          <img src="img/team3.jpg" alt="" class="card-img-top" />
+        </div>
+        <div class="card-body">
+          <h3 class="card-title">Toshar Sitory</h3>
+          <p class="card-text">UX/UI Designer</p>
+          <p class="small text-justify m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta velit libero.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<!-- Contact Us -->
+<div id="contactScroll">
+<div class="container-fluid contact-container mt-3 mt-sm-5">
+  <div class="container">
+    <div class="row contact">
+      <div class="col">
+        <h1 class="text-center mt-4 mb-5"><span>Contact Us</span></h1>
+      </div>
+    </div>
+    <form class="mx-auto bg-white p-4 mb-4 mb-sm-5">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <input type="test" name="" class="form-control" placeholder="Name" />
+        </div>
+        <div class="form-group col-md-6"> 
+          <input type="email" name="" class="form-control" placeholder="Email" />
+        </div>
+        <div class="form-group col-md-12">
+          <input type="text" name="" class="form-control" placeholder="Subject" />
+        </div>
+        <div class="col-12">
+          <textarea class="form-control" rows="4" placeholder="Your Message"></textarea>
+        </div>
+        <input type="submit" name="" class="btn btn-primary m-3 mx-auto btn-block" value="Send Message">
+      </div>
+    </form>
+  </div>
+</div>
+</div>
+
+<!-- Connect -->
+<div class="container-fluid connect">
+  <div class="row p-md-4 p-xl-5 ">
+    <div class="col-12">
+      <h1 class="text-center mb-md-4 mb-lg-5"><span>Connect</span></h1>
+    </div>
+    <div class="col-12 social px-md-3 px-xl-5 text-center">
+      <!-- Social Links -->
+      <!-- Facebook -->
+      <a href="https://www.facebook.com/artistickaran" class="mx-md-4 mx-xl-5" target="_blank">
+        <i class="fab fa-facebook-square fa-5x"></i>
+      </a>
+      <!-- Twitter -->
+      <a href="#" class="mx-md-4 mx-xl-5" target="_blank">
+        <i class="fab fa-twitter-square fa-5x"></i>
+      </a>
+      <!-- Google Plus -->
+      <a href="https://plus.google.com/107804887838671161975" class="mx-md-4 mx-xl-5" target="_blank">
+        <i class="fab fa-google-plus-g fa-5x"></i>
+      </a>
+      <!-- Instagram -->
+      <a href="https://www.instagram.com/crazzydevilz" class="mx-md-4 mx-xl-5" target="_blank">
+        <i class="fab fa-instagram fa-5x"></i>
+      </a>
+      <!-- Youtube Channel -->
+      <a href="https://www.youtube.com/channel/UCZ68HZd9DHRpW5qphz_HTrw?view_as=subscriber" class="mx-md-4 mx-xl-5" target="_blank">
+        <i class="fab fa-youtube fa-5x"></i>
+      </a>
+    </div>
+  </div>
+</div>
+
+
+<!-- Footer -->
+<div class="container-fluid copyright p-3">
+    <div class="row text-center">
+      <div class="col-12">
+        <p class="lead mb-0">All copyrights reserved &copy; 2018 | Designed &amp; Developed By: <a href="https://www.facebook.com/artistickaran" target="_blank"><span>Karan Aggarwal</span></a></p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+  <!-- Optional JavaScript -->
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
